25e9b7390d77f1da97329ce20ebfc3be0e01a853
ting-c <ting.cheong31@gmail.com>
2020-06-20 12:08:59 +0100

    .

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const db = require('./config/db');
 
+const { Pool } = require("pg");
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
+
 // Test DB
 db.authenticate()
   .then(() => console.log('database connected'))
@@ -19,6 +27,20 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => res.render('index', { layout: 'landing'}));
 
+// heroku postgres
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
+  
 app.use('/ads', require('./routes/ads'));
 
 const PORT = process.env.PORT || 5000;
